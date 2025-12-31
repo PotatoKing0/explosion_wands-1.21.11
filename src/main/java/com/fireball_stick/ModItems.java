8af11e20caf9a5c44e586f01caec4efe7c250fc0
Joachim Øen <142489531+672640@users.noreply.github.com>
2025-12-31 21:30:46 +0100

    Adding support for multiple items

---
+++ src/main/java/com/fireball_stick/ModItems.java
@@ -0,0 +1,13 @@
+package com.fireball_stick;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+
+import static net.minecraft.core.Registry.register;
+import static net.minecraft.world.item.Items.registerItem;
+
+public class ModItems {
+
+}
